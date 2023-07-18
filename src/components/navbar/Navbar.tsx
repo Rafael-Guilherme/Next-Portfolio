33899171fd71eb1863a908cfa284d3772ad4dbc4
Rafael <rafael_guilherme@hotmail.com>
2023-07-18 17:33:51 -0300

    refactor: navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,28 +1,21 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+
+import logo from '../../assets/images/logo.png'
 
 const Navbar = () => {
   return (
     <div className='flex items-center w-screen p-2 h-16 bg-white'>
         <div className='w-72'>
-            <h1 className='text-2xl font-bold text-black'>Logo</h1>
+            <Image src={logo} alt='Logotipo' />
         </div>
         <ul className='w-full flex items-center justify-around text-black'>
-            <li>
-                <Link href='/'>Home</Link>
-            </li>
-            <li>
-                <Link href='/'>About</Link>
-            </li>
-            <li>
-                <Link href='/'>Work</Link>
-            </li>
-            <li>
-                <Link href='/'>Skills</Link>
-            </li>
-            <li>
-                <Link href='/'>Contact</Link>
-            </li>
+            {['Home', 'About', 'Work', 'Skills', 'Contact'].map((item) => (
+                <li key={`${item}`}>
+                    <Link href={`#${item}`} className='text-lg font-medium'>{item}</Link>
+                </li>
+            ))}
         </ul>
     </div>
   )
