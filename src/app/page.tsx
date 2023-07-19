d05aacb68e2d22ccad0e4157c386d5ae4ba05ee6
Rafael <rafael_guilherme@hotmail.com>
2023-07-19 17:06:24 -0300

    feat: header(background)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/header/Header"
 import Navbar from "@/components/navbar/Navbar"
 
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div className="w-[1280px]">
       <Navbar />
+      <Header />
       <div className="flex items-center justify-center h-screen">
         <h1 className="text-2xl font-bold">Hello World!</h1>
       </div>
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='background-header w-screen h-screen'>
+        <div className='w-screen h-screen bg-black bg-opacity-80'>
+            <div className='flex items-center justify-center h-screen'>
+                This is the Header
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
